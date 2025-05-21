117cb5865c45fed32152d7cbcf480b5f8681ce2d
Lee JaeMin <87934576+cinnamo256@users.noreply.github.com>
2025-05-21 11:31:01 +0900

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250521/요일 맞추기/guess-day-of-week.cpp
+++ 250521/요일 맞추기/guess-day-of-week.cpp
@@ -8,7 +8,6 @@ int m1, d1, m2, d2;
 int main() {
     cin >> m1 >> d1 >> m2 >> d2;
     int month[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    string daySigunature[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
 
     int baseDay = d1;
     for (int i = 1; i < m1; i++) {
@@ -20,9 +19,21 @@ int main() {
         day += month[i];
     }
 
-    cout << daySigunature[(day - baseDay + 7) % 7];
-
-
+    if (((day - baseDay) % 7 + 7) % 7 == 0) {
+        cout << "Mon";
+    } else if (((day - baseDay) % 7 + 7) % 7 == 1) {
+        cout << "Tue";
+    } else if (((day - baseDay) % 7 + 7) % 7 == 2) {
+        cout << "Wed";
+    } else if (((day - baseDay) % 7 + 7) % 7 == 3) {
+        cout << "Thu";
+    } else if (((day - baseDay) % 7 + 7) % 7 == 4) {
+        cout << "Fri";
+    } else if (((day - baseDay) % 7 + 7) % 7 == 5) {
+        cout << "Sat";
+    } else if (((day - baseDay) % 7 + 7) % 7 == 6) {
+        cout << "Sun";
+    }
 
     return 0;
 }
