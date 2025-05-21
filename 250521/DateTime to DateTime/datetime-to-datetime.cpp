cb4a839fae394b1af323873c4f09b8e446a93a6e
Lee JaeMin <87934576+cinnamo256@users.noreply.github.com>
2025-05-21 09:32:03 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250521/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b, c;
+
+int main() {
+    cin >> a >> b >> c;
+
+    const int base = 11 + 11 * 60 + 11 * 24 * 60;
+    const int inp = c + b * 60 + a * 24 * 60;
+
+    if (base >= inp) {
+        cout << -1;
+    } else {
+        cout << inp - base;
+    }
+
+    return 0;
+}
