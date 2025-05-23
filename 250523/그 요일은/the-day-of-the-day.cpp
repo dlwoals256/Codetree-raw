3e96f69f672e322039decfa7cf558ebc6e6b48c2
Lee JaeMin <87934576+cinnamo256@users.noreply.github.com>
2025-05-23 14:12:56 +0900

    Title: 그 요일은, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250523/그 요일은/the-day-of-the-day.cpp
+++ 250523/그 요일은/the-day-of-the-day.cpp
@@ -5,43 +5,39 @@ int m1, d1, m2, d2;
 string A;
 
 int main() {
-  cin >> m1 >> d1 >> m2 >> d2;
-  cin >> A;
+    cin >> m1 >> d1 >> m2 >> d2;
+    cin >> A;
 
-  int month[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-  
-  int days1 = d1;
-  for (int i = 1; i < m1; i++) {
-      days1 += month[i];
-  }
-  
-  int days2 = d2;
-  for (int i = 1; i < m2; i++) {
-      days2 += month[i];
-  }
-  
-  int dayToNum;
-  if (A == "Mon") dayToNum = 0;
-  else if (A == "Tue") dayToNum = 1;
-  else if (A == "Wed") dayToNum = 2;
-  else if (A == "Thu") dayToNum = 3;
-  else if (A == "Fri") dayToNum = 4;
-  else if (A == "Sat") dayToNum = 5;
-  else if (A == "Sun") dayToNum = 6;
-  
-  int dayOfWeek1 = (days1 - 1) % 7;
-  
-  int daysUntilFirst = (dayToNum - dayOfWeek1 + 7) % 7;
-  
-  int firstOccurrence = days1 + daysUntilFirst;
-  
-  if (firstOccurrence > days2) {
-      cout << 0;
-      return 0;
-  }
-  
-  int count = (days2 - firstOccurrence) / 7 + 1;
-  
-  cout << count;
-  return 0;
+    int month[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    
+    int start = d1;
+    for (int i = 1; i < m1; i++) {
+        start += month[i];
+    }
+    
+    int end = d2;
+    for (int i = 1; i < m2; i++) {
+        end += month[i];
+    }
+    
+    int total_days = end - start + 1;
+
+    string week[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+    int target = 0;
+    for (int i = 0; i < 7; i++) {
+        if (week[i] == A) {
+            target = i;
+            break;
+        }
+    }
+
+    int answer = total_days / 7;
+    if (total_days % 7 > 0) {
+        int remain = total_days % 7;
+        if (target < remain) answer++;
+    }
+
+    cout << answer;
+
+    return 0;
 }
