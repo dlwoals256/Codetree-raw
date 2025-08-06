792386655ca1524900e8a8c61a9cd8d8c46b0870
Lee JaeMin <87934576+dlwoals256@users.noreply.github.com>
2025-08-06 21:54:17 +0900

    Title: 절댓값으로 변경, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250806/절댓값으로 변경/find-the-absolute-value.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[50];
+
+void abs(int* x) {
+    for (int i = 0; i < n; i++) {
+        if (*(x + i) < 0) {
+            *(x + i) = -*(x + i);
+        }
+    }
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    abs(arr);
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << ' ';
+    }
+
+    return 0;
+}
