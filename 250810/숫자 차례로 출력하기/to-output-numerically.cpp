ab3d4ad8c55dc939a674beb5e2598307e6b80452
Lee JaeMin <87934576+dlwoals256@users.noreply.github.com>
2025-08-10 10:58:37 +0900

    Title: 숫자 차례로 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250810/숫자 차례로 출력하기/to-output-numerically.cpp
+++ 250810/숫자 차례로 출력하기/to-output-numerically.cpp
@@ -6,14 +6,21 @@ int N;
 
 
 void print_col_asc(int n) {
+    int recur = n;
     if (n < 1) return;
-    print_col_asc(--n);
+    print_col_asc(--recur);
     cout << n << ' ';
 }
 
 void print_col_desc(int n) {
-    if (n > 1) return;
+    if (n < 1) return;
     cout << n-- << ' ';
+    print_col_desc(n);
+}
+
+void print_row(int n) {
+    print_col_asc(n);
+    cout << '\n';
     print_col_desc(n);
 }
 
