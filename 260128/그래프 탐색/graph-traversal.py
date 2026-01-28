41ee40b05826e505a6d3416d4d4855bd0c7dec8c
Code hero <87934576+dlwoals256@users.noreply.github.com>
2026-01-28 13:12:14 +0900

    Title: 그래프 탐색, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260128/그래프 탐색/graph-traversal.py
+++ 260128/그래프 탐색/graph-traversal.py
@@ -1,22 +1,25 @@
 n, m = map(int, input().split())
 edges = [tuple(map(int, input().split())) for _ in range(m)]
 
-graph = [[0 for _ in range(n)] for _ in range(n)]
+if m == 0:
+    print(0)
+else:
+    graph = [[0 for _ in range(n)] for _ in range(n)]
 
-for x, y in edges:
-    graph[x-1][y-1] = 1
-    graph[y-1][x-1] = 1
+    for x, y in edges:
+        graph[x-1][y-1] = 1
+        graph[y-1][x-1] = 1
 
-visited = [False for _ in range(n)]
-cnt = 0
-def dfs(node):
-    global cnt
-    for curr_node in range(n):
-        if graph[node][curr_node] and not visited[curr_node]:
-            cnt += 1
-            visited[curr_node] = True
-            dfs(curr_node)
+    visited = [False for _ in range(n)]
+    cnt = 0
+    def dfs(node):
+        global cnt
+        for curr_node in range(n):
+            if graph[node][curr_node] and not visited[curr_node]:
+                cnt += 1
+                visited[curr_node] = True
+                dfs(curr_node)
 
-dfs(1)
+    dfs(1)
 
-print(cnt-1)
+    print(cnt-1)
