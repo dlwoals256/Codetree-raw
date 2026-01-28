fa0feaa30916d4d0bea1057eb4a4cda71de06e9e
Code hero <87934576+dlwoals256@users.noreply.github.com>
2026-01-28 11:44:12 +0900

    Title: 행복한 수열의 개수, Time: 62ms, Memory: 17MB, Status: Passed - Codetree

--- 260128/행복한 수열의 개수/number-of-happy-sequence.py
+++ 260128/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,29 +1,32 @@
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-def is_happy_sequence(seq):
+def is_happy(sequence):
+    if n < m: return False
+    
     cnt = 1
-    max_cnt = 1
     for i in range(1, n):
-        if seq[i] == seq[i-1]:
+        if sequence[i] == sequence[i-1]:
             cnt += 1
         else:
             cnt = 1
-        max_cnt = max(max_cnt, cnt)
-
-    return max_cnt >= m
+        
+        if cnt >= m:
+            return True
+            
+    return cnt >= m
 
 if m == 1:
     print(n * 2)
 else:
     happy_cnt = 0
     for i in range(n):
-        if is_happy_sequence(grid[i]):
+        if is_happy(grid[i]):
             happy_cnt += 1
 
     for j in range(n):
         col_seq = [grid[i][j] for i in range(n)]
-        if is_happy_sequence(col_seq):
+        if is_happy(col_seq):
             happy_cnt += 1
     
     print(happy_cnt)
